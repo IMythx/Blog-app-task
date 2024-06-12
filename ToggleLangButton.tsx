fda8027a92efcdc91df23f70a384059b93b23c67
IMythx <88852348+IMythx@users.noreply.github.com>
2024-06-12 18:41:04 +0300

    fix: search params on home page logo redirection

--- ToggleLangButton.tsx
+++ ToggleLangButton.tsx
@@ -2,7 +2,7 @@
 import { usePathname, useRouter } from "@/navigation";
 import { useLocale, useTranslations } from "next-intl";
 import { useTransition } from "react";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 import { useParams } from "next/navigation";
 
 const ToggleLangButton = () => {
